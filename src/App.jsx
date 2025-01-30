593e583f06db1bb125266198ca4717846e08db25
hasibul217 <shantossnic@gmail.com>
2025-01-30 16:14:28 +0600

    All file added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl  underline text-red-700">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
